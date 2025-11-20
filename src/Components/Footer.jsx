b12943dae32d21647056426dfa157bb4b9351c99
DeepakLinux <deepakvishwakarmajs@gmail.com>
2025-11-20 21:00:39 +0530

    Border,position style update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { Eye, TrendingUp, Users } from "lucide-react"
 export default function Footer()
 {
 return (
-    <div className="flex md:flex-row flex-col justify-between  p-1  border-l-0 border-r-0 border-b-2 border-black">
+    <div className="flex md:flex-row flex-col justify-between  sticky bottom-0 bg-white p-1  border-l-0 border-r-0 border-b-0 border-t border-gray-400">
         <div className="flex gap-2 flex-wrap lg:flex-nowrap lg:gap-10 p-2">
            <IconBoxWithText Icon={TrendingUp} text="Total Today" number={255}/>
            <IconBoxWithText Icon={Users} color={"#32CD32"} text="Active" number={5}/>
