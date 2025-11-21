34f2ddbdcd7464caa0bbfe0d0e513db53bda38b7
DeepakLinux <deepakvishwakarmajs@gmail.com>
2025-11-21 17:03:24 +0530

    Component Add

---
+++ src/Components/HomeCardHeading.jsx
@@ -0,0 +1,11 @@
+const HomeCardHeading = ({ HeadingCard }) => {
+    return (
+         <div className="flex justify-center items-center p-2">
+                    <h2 className="text-2xl font-medium">
+                       {HeadingCard}
+                    </h2>
+                </div>
+    )
+};
+
+export default HomeCardHeading;
