a0b4acef059719e7c2e2b4ca176f2911e4dd8fa3
DeepakLinux <deepakvishwakarmajs@gmail.com>
2025-11-21 12:18:36 +0530

    text center property add

--- src/Components/SubDescripton.jsx
+++ src/Components/SubDescripton.jsx
@@ -3,7 +3,7 @@ export default function SubDescription({subtext})
     return (
         <div className="grid place-content-center p-4">
         <div className="flex justify-center items-center">
-          <h1 className="text-xl text-gray-400 flex  gap-2 justify-center items-center"> 
+          <h1 className="text-xl text-center text-gray-400 flex  gap-2 justify-center items-center"> 
             {subtext}
           </h1>
         </div>
