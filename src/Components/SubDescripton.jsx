bac507b7d7877a2a34936c9bd2181784f90993ed
DeepakLinux <deepakvishwakarmajs@gmail.com>
2025-11-21 17:01:59 +0530

    add a new prop

--- src/Components/SubDescripton.jsx
+++ src/Components/SubDescripton.jsx
@@ -1,10 +1,12 @@
-export default function SubDescription({subtext})
+import { Eye } from "lucide-react";
+
+export default function SubDescription({subtext,isIcon=false})
 {
     return (
         <div className="grid place-content-center p-4">
         <div className="flex justify-center items-center">
           <h1 className="text-xl text-center text-gray-400 flex  gap-2 justify-center items-center"> 
-            {subtext}
+       {isIcon && <Eye/>} {subtext}
           </h1>
         </div>
       </div> 
