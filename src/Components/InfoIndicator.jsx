95477af9beb34c366c37b4418e65f7ca476f3a60
DeepakLinux <deepakvishwakarmajs@gmail.com>
2025-11-21 17:01:30 +0530

    New Component Add

---
+++ src/Components/InfoIndicator.jsx
@@ -0,0 +1,12 @@
+function InfoIndicator()
+{
+    return (
+         <div className="grid place-content-center">
+   <div className="w-dvh h-24 flex rounded-2xl justify-center items-center  bg-base-300 text-gray-400">
+       💡 Hover over sections to highlight them for all visitors. Add emoji marks to share your reactions!
+   </div>
+  </div>
+    )
+}
+
+export default InfoIndicator
