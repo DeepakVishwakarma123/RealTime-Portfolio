17898729cd1a584c044a3139f3e7f3ace14c696f
DeepakLinux <deepakvishwakarmajs@gmail.com>
2025-11-21 12:15:10 +0530

    fixed padding

--- src/Components/PageBigHeading.jsx
+++ src/Components/PageBigHeading.jsx
@@ -1,8 +1,8 @@
 export default function PageBigHeading({bigText})
 {
 return (
-      <div className="grid place-content-center p-4">
-        <div className="flex justify-center items-center p-4">
+      <div className="grid place-content-center pt-2">
+        <div className="flex justify-center items-center ">
           <h1 className="text-5xl sm:text-7xl">{bigText}</h1>
         </div>
       </div>
