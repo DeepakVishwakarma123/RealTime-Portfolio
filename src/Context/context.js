1e5de77c9158537f62adcea05f5d76fe83a87d0b
DeepakLinux <deepakvishwakarmajs@gmail.com>
2025-11-26 01:56:43 +0530

    Context Create-

---
+++ src/Context/context.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+const ActiveUserOnpage=createContext()
+
+ const ThemeContext=createContext()
+
+
+export default ActiveUserOnpage
+
+export {ThemeContext}
