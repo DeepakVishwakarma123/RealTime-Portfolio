da5ec80be4c26f3f929d7b944113719340b4f570
DeepakLinux <deepakvishwakarmajs@gmail.com>
2025-11-26 01:57:55 +0530

    package json or tailwind modify add

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // Uses system preference instead of class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
